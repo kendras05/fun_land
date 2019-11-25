0889dfa9605188536fbfe0b2929efa26ce651972
user <user01@KMS-MacBook.local>
2019-11-25 06:24:56 -0800

    changed code in rb for file.write, trying to get random information in each line

--- fun_land.rb
+++ fun_land.rb
@@ -2,14 +2,17 @@ File.write("rewards_seeds.txt", "w")
 
 require 'faker'
 
+
+
 title = Faker::Creature::Animal.name
+title_= [title]* ","
 tokens_num = Random.rand(5...5000)
 available = Faker::Boolean.boolean(true_ratio: 0.5)
 
 
 75.times do |i|
 
-File.write("rewards_seeds.txt", "INSERT INTO rewards (item_name, tokens, available) VALUES (#{title}, #{tokens_num}, #{available});")
+File.write("rewards_seeds.txt", "INSERT INTO rewards (item_name, tokens, available) VALUES ('#{title_}', #{tokens_num}, #{available});")
 
 end 
 
@@ -22,7 +25,7 @@ tokens_hav = Random.rand(0...10000)
 
 250.times do |i|
 
-File.write("guests_seed.txt", "INSERT INTO guests (name, tokens) VALUES (#{namefak}, #{tokens_hav});")
+File.write("guests_seed.txt", "INSERT INTO guests (name, tokens) VALUES ('#{namefak}', #{tokens_hav});")
 
 end
 
