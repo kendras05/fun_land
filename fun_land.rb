88b823f6d4e200f1664a025b06ba1b920565c151
user <user01@KMS-MacBook.local>
2019-11-24 17:47:27 -0800

    trying to work out number of characters error in psql when title is less than 20 characters

--- fun_land.rb
+++ fun_land.rb
@@ -1,10 +1,10 @@
-File.open("rewards_seeds.txt")
+File.write("rewards_seeds.txt", "w")
 
 require 'faker'
 
-item_name = Faker::Book.title
-tokens = Random.rand(5...5000)
-availabile = Boolean
+title = Faker::Creature::Animal.name
+tokens_num = Random.rand(5...5000)
+available = Faker::Boolean.boolean(true_ratio: 0.5)
 
 
 75.times do |i|
@@ -13,7 +13,17 @@ File.write("rewards_seeds.txt", "INSERT INTO rewards (item_name, tokens, availab
 
 end 
 
+File.write("guests_seed.txt", "w")
 
+require 'faker'
 
+namefak = Faker::Name.name
+tokens_hav = Random.rand(0...10000)
+
+250.times do |i|
+
+File.write("guests_seed.txt", "INSERT INTO guests (name, tokens) VALUES (#{namefak}, #{tokens_hav});")
+
+end
 
 
